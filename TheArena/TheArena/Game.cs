529755e60dae56afe7f1d0aab66e83a89102ac37
Seth Kitchen <sjkyv5@mst.edu>
2018-09-11 00:16:40 -0500

    Tournament Generation

---
+++ TheArena/TheArena/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheArena
+{
+    public class Game
+    {
+        public List<Player> Competitors;
+        public bool IsRunning=false;
+        public bool IsComplete=false;
+        public int RoundNumber;
+        public string Game_URL;
+        
+        public Game()
+        {
+            Competitors = new List<Player>();
+        }
+
+        public Player GetParentOfPlayersInThisGame()
+        {
+            if (Competitors.Count > 0)
+            {
+                return Competitors[0].ParentNode;
+            }
+            return null;
+        }
+
+        public void SetWinner(Player champion, Tournament t, string name, string url)
+        {
+            IsComplete = true;
+            IsRunning = false;
+            Game_URL = url;
+            if (Competitors.Count > 0)
+            {
+                Competitors[0].ParentNode.Name=name;
+            }
+            champion.Traverse(t.GetGames);
+        }
+
+        public bool ContainsPlayer(Player p)
+        {
+            return Competitors.Contains(p);
+        }
+
+        public void AddPlayer(Player p)
+        {
+            Competitors.Add(p);
+        }
+    }
+}
