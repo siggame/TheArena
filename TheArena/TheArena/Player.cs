dc0f7caf1911862307d0c18dc9c1c069220fde49
Seth Kitchen <sjkyv5@mst.edu>
2018-09-14 22:05:38 -0500

    just let me pull

---
+++ TheArena/TheArena/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheArena
+{
+    public class Player : GenericTree<Player> // concrete derivation 
+    { 
+        public string Name { get; set; }
+
+        public Player ParentNode { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
