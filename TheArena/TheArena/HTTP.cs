55cb40c3724f5d673bcf7bee23aa4a2cdf25e198
Seth Kitchen <sjkyv5@mst.edu>
2019-04-13 14:47:45 -0500

    async method not returning should be fixed

--- TheArena/TheArena/HTTP.cs
+++ TheArena/TheArena/HTTP.cs
@@ -53,7 +53,7 @@ namespace TheArena
         public int numberOfPlayers;
         public List<Client> clients = new List<Client>();
     }
-        
+
 
     public class Winner
     {
@@ -71,7 +71,7 @@ namespace TheArena
     {
         public static bool HTTPPostStartGame(string gameName, string session, gameSettings gameSettings)
         {
-            MyPacket p = new MyPacket() { gameName = gameName, session = session, gameSettings = gameSettings};
+            MyPacket p = new MyPacket() { gameName = gameName, session = session, gameSettings = gameSettings };
             string serialized = JsonConvert.SerializeObject(p);
             Log.TraceMessage(Log.Nav.NavIn, "Serialized data: " + serialized, Log.LogType.Info);
             using (var client = new HttpClient())
@@ -80,13 +80,15 @@ namespace TheArena
                 {
                     List<Task> allGames = new List<Task>();
                     //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IklMaWtlU29ja3NPblN1bmRheXMiLCJpZCI6Mywicm9sZSI6InVzZXIiLCJpYXQiOjE1NDE4NjY1NTcsImV4cCI6MTU0MjI5ODU1N30.XWaWB_cWhUFEC1m0GxFJ4ln8uq5h086gXGxRmOLVXA0");
-                    allGames.Add(Task.Run(async () => {var x = await client.PostAsync(
-                        "http://127.0.0.1:3080/setup",
-                         new StringContent(serialized, Encoding.UTF8, "application/json"));
-                         Console.WriteLine(await x.Content.ReadAsStringAsync());
-                         Log.TraceMessage(Log.Nav.NavIn, "Server Response Content: " + await x.Content.ReadAsStringAsync(), Log.LogType.Info);
-                         Log.TraceMessage(Log.Nav.NavIn, "Server Response: " + x, Log.LogType.Info);
-                         }));
+                    allGames.Add(Task.Run(async () =>
+                    {
+                        var x = await client.PostAsync(
+"http://127.0.0.1:3080/setup",
+new StringContent(serialized, Encoding.UTF8, "application/json"));
+                        Console.WriteLine(await x.Content.ReadAsStringAsync());
+                        Log.TraceMessage(Log.Nav.NavIn, "Server Response Content: " + await x.Content.ReadAsStringAsync(), Log.LogType.Info);
+                        Log.TraceMessage(Log.Nav.NavIn, "Server Response: " + x, Log.LogType.Info);
+                    }));
                     Task.WaitAll(allGames.ToArray());
                     return true;
                 }
@@ -109,34 +111,38 @@ namespace TheArena
                     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNldGhBZG1pbiIsImlkIjo0LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE1NTUwMjU0NDQsImV4cCI6MTU1NTQ1NzQ0NH0.m4bOU5k5hH2YTnDT0094oDA1XDHxsQqMxNkSQQFCaHE");
                     //build url
                     string url = "http://127.0.0.1:3080/status/" + gameName + "/" + gameSession;
-                    allGames.Add(Task.Run(async () => {var x = await client.GetAsync(url);
-                         string response = await x.Content.ReadAsStringAsync();
-                         Console.WriteLine(response);
-                         Log.TraceMessage(Log.Nav.NavIn, "Server Response Content: " + response, Log.LogType.Info);
-                         Log.TraceMessage(Log.Nav.NavIn, "Server Response: " + x, Log.LogType.Info);
-                         Result deserialized;
+                    allGames.Add(Task.Run(async () =>
+                    {
+                        var x = await client.GetAsync(url);
+                        string response = await x.Content.ReadAsStringAsync();
+                        Console.WriteLine(response);
+                        Log.TraceMessage(Log.Nav.NavIn, "Server Response Content: " + response, Log.LogType.Info);
+                        Log.TraceMessage(Log.Nav.NavIn, "Server Response: " + x, Log.LogType.Info);
+                        Result deserialized;
 
-                         try
-                         {
+                        try
+                        {
                             deserialized = JsonConvert.DeserializeObject<Result>(response);
                             Console.WriteLine(deserialized.clients);
-                         }
-                         catch(Exception k)
-                         {
-                             string except = k.Message;
-                             Log.TraceMessage(Log.Nav.NavIn, "Except: " + k, Log.LogType.Info);
-                             Console.WriteLine("Exception. Check Log.");
-                             return null;
-                         }
+                        }
+                        catch (Exception k)
+                        {
+                            string except = k.Message;
+                            Log.TraceMessage(Log.Nav.NavIn, "Except: " + k, Log.LogType.Info);
+                            Console.WriteLine("Exception. Check Log.");
+                            return null;
+                        }
 
-                         //to remove warnings on compile...
-                         string trash = deserialized.gameSession;
-                         string trash1 = deserialized.gameName;
-                         int trash2  = deserialized.numberOfPlayers;
-                         
-                         return deserialized;
-                         }));
+                        //to remove warnings on compile...
+                        string trash = deserialized.gameSession;
+                        string trash1 = deserialized.gameName;
+                        int trash2 = deserialized.numberOfPlayers;
+
+                        return deserialized;
+                    }));
                     Task.WaitAll(allGames.ToArray());
+                    Log.TraceMessage(Log.Nav.NavOut, "all games array of tasks finished with result: "+(allGames[0] as Task<Result>).Result, Log.LogType.Info);
+                    return (allGames[0] as Task<Result>).Result;
                 }
                 catch (Exception ex)
                 {
@@ -145,12 +151,11 @@ namespace TheArena
                     return null;
                 }
             }
-            return null;
         }
 
         public static void HTTPPostSendToWeb(string status, string winReason, string loseReason, string logURL, string winnerTeamName, string winnerVersion, string loserTeamName, string loserVersion)
         {
-            MyPacket p = new MyPacket() { status = status, loseReason = loseReason, winReason = winReason, logUrl = logURL, winner=new Winner() { teamName = winnerTeamName, version = winnerVersion },loser=new Loser() { teamName = loserTeamName, version = loserVersion } };
+            MyPacket p = new MyPacket() { status = status, loseReason = loseReason, winReason = winReason, logUrl = logURL, winner = new Winner() { teamName = winnerTeamName, version = winnerVersion }, loser = new Loser() { teamName = loserTeamName, version = loserVersion } };
             string serialized = JsonConvert.SerializeObject(p);
             Console.WriteLine(serialized);
             using (var client = new HttpClient())
